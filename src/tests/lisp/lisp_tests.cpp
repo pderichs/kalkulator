9d88fdb851cdf7a08a2893ea9dd46d846773a5ba
pderichs <derichs@posteo.de>
2023-07-20 11:14:41 +0200

    Add test

--- src/tests/lisp/lisp_tests.cpp
+++ src/tests/lisp/lisp_tests.cpp
@@ -8,10 +8,12 @@
 
 int run_lisp_tests_parsing1();
 int run_lisp_tests_parsing2();
+int run_lisp_tests_parsing3();
 
 int run_lisp_tests() {
   RUN_TEST(run_lisp_tests_parsing1);
   RUN_TEST(run_lisp_tests_parsing2);
+  RUN_TEST(run_lisp_tests_parsing3);
 
   return 0;
 }
@@ -69,6 +71,39 @@ int run_lisp_tests_parsing2() {
     TEST_ASSERT(d == 4.0);
     TEST_ASSERT(tokens[8].id == CLOSE_BRACKET);
     TEST_ASSERT(tokens.size() == 9);
+  } catch (LispParserError &lpe) {
+    std::cerr << "*** Caught lisp parser error: " << lpe.what() << " (item: \""
+              << lpe.item() << "\")" << std::endl;
+
+    TEST_ASSERT(false);
+  }
+
+  return 0;
+}
+
+int run_lisp_tests_parsing3() {
+  LispParser parser("(hello \"8282\" -484.32)");
+
+  try {
+    LispTokens tokens = parser.parse();
+
+    //std::cerr << "Tokens size: " << tokens.size() << std::endl;
+
+    TEST_ASSERT(tokens[0].id == OPEN_BRACKET);
+    TEST_ASSERT(tokens[1].id == IDENTIFIER);
+    std::string s = std::any_cast<std::string>(tokens[1].content);
+    TEST_ASSERT(s == "hello");
+    TEST_ASSERT(tokens[2].id == SPACE);
+    TEST_ASSERT(tokens[3].id == STRING);
+    s = std::any_cast<std::string>(tokens[3].content);
+    std::cerr << s << std::endl;
+    TEST_ASSERT(s == "8282");
+    TEST_ASSERT(tokens[4].id == SPACE);
+    TEST_ASSERT(tokens[5].id == NUMBER);
+    double d = std::any_cast<double>(tokens[5].content);
+    TEST_ASSERT(d == -484.32);
+    TEST_ASSERT(tokens[6].id == CLOSE_BRACKET);
+    TEST_ASSERT(tokens.size() == 7);
   } catch (LispParserError &lpe) {
     std::cerr << "*** Caught lisp parser error: " << lpe.what() << " (item: \""
               << lpe.item() << "\")" << std::endl;
