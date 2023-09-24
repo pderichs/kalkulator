3c3320755d7c1b10f392d8c04c4c29ce36a5db26
pderichs <derichs@posteo.de>
2023-09-24 10:17:26 +0200

    Fix and function, add test for and

--- README.md
+++ README.md
@@ -45,9 +45,8 @@ This project makes use of other external libraries and projects. Please refer to
 
 ## TODO List
 
-- BUG: More tests for or
-- BUG: More tests for xor
 - BUG: More tests for and
+- BUG: More tests for xor
 - BUG: googletest has the possibility to generate test cases on the fly - replace our current map-based solution with that (see e.g. execute_lisp_tests)
 - BUG: Cell does not check for numbers (expect_number)
 - FEAT: Mulitiple cell selection
@@ -89,6 +88,7 @@ This project makes use of other external libraries and projects. Please refer to
 
 # DONE
 
+- BUG: More tests for or
 - BUG: More tests for if
 - REFA: Design: Function contexts are always calling extract_params_from_list, then execute_functions_and_extract_list_results
 - REFA: eval tests refactoring
--- tests/src/lisp/lisp_and_tests.cpp
+++ tests/src/lisp/lisp_and_tests.cpp
@@ -1,24 +1,28 @@
 /**
-* Kalkulator - a simple and small spread sheet app.
-* Copyright (C) 2023  pderichs (derichs@posteo.de)
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+ * Kalkulator - a simple and small spread sheet app.
+ * Copyright (C) 2023  pderichs (derichs@posteo.de)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
-#include "model/lisp/lisp_execution_context.h"
-#include "model/lisp/lisp_value_factory.h"
 #include "lisp_test_tools.h"
+#include "mock_function_context.h"
+#include "model/lisp/lisp_execution_context.h"
+#include "model/lisp/lisp_parser.h"
+#include "model/lisp/lisp_value_factory.h"
+#include "model/lisp/lisp_value_parser.h"
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 TEST(LispAndTests, AndTest1) {
@@ -31,5 +35,31 @@ TEST(LispAndTests, AndTest1) {
       {"(and (= 1 1) (= 1 1))", LispValueFactory::new_bool(LISP_BOOL_TRUE)},
   };
 
+  // FIXME Generate separate test cases: see googletest documentation
   return execute_lisp_tests(tests, "and");
+}
+TEST(LispAndTests, AndDoesNotExecuteOtherConditionIfFirstIsFalseAlready) {
+  LispParser parser("(and (= 4 4 6) (lambda (x y) (+ x y)))");
+
+  LispTokens tokens;
+  EXPECT_NO_THROW(tokens = parser.parse());
+
+  LispValueParser value_parser(tokens);
+
+  auto value = value_parser.next();
+  EXPECT_TRUE(value);
+  EXPECT_TRUE(value->is_function());
+
+  std::shared_ptr<MockFunctionContext> fct =
+      std::make_shared<MockFunctionContext>();
+  LispExecutionContext executor;
+  executor.add_function("lambda", fct, true);
+
+  // We want to make sure the lambda is not executed
+  EXPECT_CALL(*fct, value).Times(0);
+
+  LispValuePtr result = executor.execute(value, {});
+
+  EXPECT_TRUE(result->is_boolean());
+  EXPECT_EQ(result->boolean(), LISP_BOOL_FALSE);
 }
