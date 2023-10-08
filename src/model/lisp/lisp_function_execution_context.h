45c20a09c9f5acbe7bde33a332714b0d17120adc
pderichs <derichs@posteo.de>
2023-10-08 08:35:54 +0200

    Add comments

--- src/model/lisp/lisp_function_execution_context.h
+++ src/model/lisp/lisp_function_execution_context.h
@@ -21,41 +21,102 @@
 
 #include <cstddef>
 #include <memory>
-
 #include "lisp_execution_context.h"
 #include "lisp_execution_context_error.h"
 #include "lisp_value.h"
 
+/**
+ * Base class for all native lisp function implementations.
+ */
 class LispFunctionExecutionContext {
 public:
   LispFunctionExecutionContext() = default;
   virtual ~LispFunctionExecutionContext() = default;
 
+  /**
+   * Executes the function and returns the result.
+   *
+   * @param func Function to be executed
+   * @param execution_context Execution context to be used for running the function
+   * @param context_param Generic parameter for each execution
+   * @param update_id Identifies the current update operation
+   * @return Result of the operation
+   */
   virtual LispValuePtr value(const LispValuePtrVector &func,
                              const LispExecutionContext &execution_context,
                              const std::any &context_param,
                              UpdateIdType update_id) = 0;
 
 protected:
+  /**
+   * Executes possible function calls within provided list
+   *
+   * @param params List to be executed
+   * @param execution_context Execution context used for this operation
+   * @param context_param Generic parameter for each execution
+   * @param update_id Identifies the current update operation
+   * @return List containing function execution results
+   */
   LispValuePtrVector
   execute_functions(const LispValuePtrVector &params,
                     const LispExecutionContext &execution_context,
                     const std::any &context_param,
                     UpdateIdType update_id) const;
+
+  /**
+   * Executes all functions and expands the lists
+   *
+   * @param params Parameters to operate on
+   * @param execution_context Execution context used for this operation
+   * @param context_param Generic parameter for each execution
+   * @param update_id Identifies the current update operation
+   * @return List which contains the function results and extracted list items
+   */
   LispValuePtrVector execute_functions_and_extract_list_results(
       const LispValuePtrVector &params,
       const LispExecutionContext &execution_context,
       const std::any &context_param, UpdateIdType update_id) const;
 
+  /**
+   * Ensures the provided function call contains parameters.
+   * @param func List to be checked
+   */
   void ensure_params(const LispValuePtrVector &func) const;
+
+  /**
+   * Ensures the provided value is representing a number. Also converts e.g.
+   * none to 0.
+   *
+   * @param value Value to be checked
+   * @param execution_context Execution context used for this operation
+   * @param context_param Generic parameter for each execution
+   * @param update_id Identifies the current update operation
+   * @return Value in number representation
+   */
   LispValuePtr expect_number(const LispValuePtr &value,
                              const LispExecutionContext &execution_context,
                              const std::any &context_param,
                              UpdateIdType update_id) const;
 
+  /**
+   * Returns the parameters of a function
+   *
+   * @param func List representing a function call
+   * @return Parameter list of the function call
+   */
   LispValuePtrVector
   extract_params_from_list(const LispValuePtrVector &func) const;
 
+  /**
+   * Helper function which combines the functionality of extract_params_from_list
+   * and execute_functions_and_extract_list_results.
+   *
+   * @param func Function to operate on
+   * @param execution_context Execution context used for this operation
+   * @param context_param Generic parameter for each execution
+   * @param update_id Identifies the current update operation
+   * @return List containing function results and extracted items
+   */
   LispValuePtrVector
   extract_and_execute_params(const LispValuePtrVector &func,
                              const LispExecutionContext &execution_context,
